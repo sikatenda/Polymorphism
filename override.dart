b3f77171189d512702eef982c4c36c829955e721
Jacques Feruzi <jferuzi019@gmail.com>
2024-07-05 17:55:42 +0200

    user input

--- override.dart
+++ override.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 main() {
+  //variable
+  var name, number;
+
+  //prompting the user for input
+  print("Please enter the  name");
+  name = stdin.readLineSync();
+  print("please enter the age!");
+  number = stdin.readLineSync();
+
+  //declare child class
   Child c = Child();
-  c.name = "FERUZI";
-  c.age = 11;
+  c.name = name;
+  c.age = int.parse(number);
   c.display();
 }
 
